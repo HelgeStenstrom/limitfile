f21ffa790a82d982b9ef66ebf9b9bdc84d5d510a
Helge Stenström <h.stenstrom@gmail.com>
2017-03-09 19:14:38 +0100

    pipeSepList passes first test

--- test_OneTeSyntax.hs
+++ test_OneTeSyntax.hs
@@ -2,7 +2,13 @@ module Main
 where
 import Test.HUnit
 import OneTeSyntax
+import Text.Parsec (parse)
 
 main = do runTestTT tests
 
-tests = TestList []
+tests = TestList [
+    findWordBeforePipe
+    ]
+
+findWordBeforePipe = 
+    parse pipeSepList "" "abc|def" ~?= Right ["abc", "def"]
